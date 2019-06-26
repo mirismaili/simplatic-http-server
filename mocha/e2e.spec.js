86b769060c52b9e1d0a3eb7b4456c9b2adc639d4
S. M. Mir-Ismaili <s.m.mirismaili@gmail.com>
2019-06-26 14:59:37 +0430

    test(e2e): Was upgraded to cover more things (a '.js' and a '.css' file were added to "index.html")

--- mocha/e2e.spec.js
+++ mocha/e2e.spec.js
@@ -8,7 +8,7 @@ const puppeteer = require('puppeteer')
 const {expect} = require('chai')
 const StaticServer = require('../dist/main.umd.js')
 
-const mockPageUrl = 'http://127.0.0.1:3000/mocha/stub'
+const mockPageUrl = 'http://127.0.0.1:3000/mocha/mock/index.html'
 
 const staticServer = new StaticServer(3000)
 
@@ -18,11 +18,11 @@ before(async () => {
 	
 	const launchPuppeteerPromise = new Promise((resolve, reject) => {
 		puppeteer.launch(
-				{
-					// headless: false,
-					// // slowMo: 500,
-					// devtools: true,
-				}
+				// {
+				// 	headless: false,
+				// 	// slowMo: 500,
+				// 	devtools: true,
+				// }
 		).then(browser => {
 			global.browser = browser
 			resolve()
@@ -33,8 +33,8 @@ before(async () => {
 	await Promise.all([staticServer.listen(), launchPuppeteerPromise])
 })
 
-describe('sample test', () => {
-	it('should work', async () => {
+describe('End-to-end tests using puppeteer', () => {
+	it('Should load mock page as expected and without any error', async () => {
 		const page = (await browser.pages())[0];
 		
 		const errors = []
@@ -46,9 +46,16 @@ describe('sample test', () => {
 		
 		await page.goto(mockPageUrl)
 		
-		const pageTitle = await page.title()
+		expect(await page.title()).to.equal('Mock HTML')
 		
-		expect(pageTitle).to.equal('Mock HTML')
+		expect(await page.evaluate(() => [
+			testEl.innerHTML,
+			window.getComputedStyle(testEl).getPropertyValue('font-size'),
+		])).to.eql([
+			'Hello World!',
+			'100px',
+		])
+		
 		expect(errors).to.be.empty
 	})
 })
---
+++ mocha/mock/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created at 1398/4/4 (2019/6/25).
+ * @author {@link https://mirismaili.github.io S. Mahdi Mir-Ismaili}
+ */
+'use strict'
+
+let testEl
+
+window.onload = () => {
+	testEl = document.getElementById('test')
+	testEl.innerHTML = 'Hello World!'
+}
